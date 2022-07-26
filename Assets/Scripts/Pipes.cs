33b924e9dce8898dbfd7ef53e33018df81fbb17c
Artisan Jenkins <artisan.jenkins@The-Hidden-Genius-Project-2.local>
2022-07-26 12:51:25 -0700

    Birdy

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float speed = 5f;
+    private float leftEdge;
+
+
+    private void Update() {
+
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        
+        if (transform.position.x < leftEdge) {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void Start() {
+
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+    }
+
+
+
+
+}
